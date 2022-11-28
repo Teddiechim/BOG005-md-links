b56bc6e5024f060281f5c18e3e7824a408ef130d
Teddiechim <107264908+Teddiechim@users.noreply.github.com>
2022-11-28 02:31:01 -0500

    Test unitario mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,10 @@
-const mdLinks = require('../');
+const { mdLinks } = require("../index.js");
+const { linksArray } = require("./test.js");
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe("mdLinks", () => {
+  it("mdLinks returns an array of objects with the links of the directory", async () => {
+    const path = await mdLinks("./archivos");
+    console.log(path);
+    expect(path).toEqual(linksArray);
   });
-
 });
---
+++ test/test.js
@@ -0,0 +1,26 @@
+const linksArray = [
+  {
+    href: "https://curriculum.laboratoria.la/es/topics/javascript/02-flow-control/03-functions",
+    text: "1",
+    file: "./archivos/1.md",
+  },
+  {
+    href: "https://curriculum.laboratoria.la/es/topics/javascript/02-flow-control/03-functions",
+    text: "2",
+    file: "./archivos/2.md",
+  },
+  {
+    href: "https://curriculum.laboratoria.la/es/topics/javascript/02-flow-control/03-functions",
+    text: "3",
+    file: "./archivos/a/3.md",
+  },
+  {
+    href: "https://curriculum.laboratoria.la/es/topics/javascript/02-flow-control/03-functions",
+    text: "4",
+    file: "./archivos/a/b/4.md",
+  },
+];
+
+module.exports = {
+  linksArray,
+};
